63c5f214369064efb2eb692078a8a21d7c4d6e1b
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-09-25 13:13:53 +0200

    Add button to storyboard

---
+++ Flo/Flo/PushButton.swift
@@ -0,0 +1,21 @@
+//
+//  PushButton.swift
+//  Flo
+//
+//  Created by Carlos Cortés Sánchez on 25/09/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class PushButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
