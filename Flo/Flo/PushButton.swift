15e5854d12fd5471fd61ec3a28997aab08f28b49
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-09-25 13:42:21 +0200

    Draw button

--- Flo/Flo/PushButton.swift
+++ Flo/Flo/PushButton.swift
@@ -8,14 +8,56 @@
 
 import UIKit
 
-class PushButton: UIButton {
+@IBDesignable class PushButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private struct Constants {
+        static let plusLineWidth: CGFloat = 3.0
+        static let plusButtonScale: CGFloat = 0.6
+        static let halfPointShift: CGFloat = 0.5
     }
-    */
+    
+    private var halfWidth: CGFloat {
+        return bounds.width/2
+    }
+    
+    private var halfHeight: CGFloat {
+        return bounds.height/2
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(ovalIn: rect)
+        UIColor.blue.setFill()
+        path.fill()
+        
+        //setup the height and width variables
+        //for the horizontal stroke
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * Constants.plusButtonScale
+        let halfPlusWidth = plusWidth/2
+        
+        //create the path
+        let plusPath = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        plusPath.lineWidth = Constants.plusLineWidth
+        
+        //Horizontal line
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        plusPath.move(to: CGPoint(x: halfWidth - halfPlusWidth + Constants.halfPointShift, y: halfHeight + Constants.halfPointShift))
+        
+        //add a point to the path at the end of the stroke
+        plusPath.addLine(to: CGPoint(x: halfWidth + halfPlusWidth + Constants.halfPointShift, y: halfHeight + Constants.halfPointShift))
+        
+        //Vertical line
+        plusPath.move(to: CGPoint(x: halfWidth + Constants.halfPointShift, y: halfHeight - halfPlusWidth + Constants.halfPointShift))
+        
+        plusPath.addLine(to: CGPoint(x: halfWidth + Constants.halfPointShift, y: halfHeight + halfPlusWidth + Constants.halfPointShift))
+        
+        //set the stroke color
+        UIColor.white.setStroke()
+        
+        //draw the stroke
+        plusPath.stroke()
+    }
 
 }
