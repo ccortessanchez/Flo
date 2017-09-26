b2dabe6c000a37da6f24e5e770ae2c4ef3486fd0
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-09-26 11:39:39 +0200

    Animated transition between graph/counter views

--- Flo/Flo/ViewController.swift
+++ Flo/Flo/ViewController.swift
@@ -16,12 +16,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var graphView: GraphView!
     
+    var isGraphViewShowing = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         counterLabel.text = String(counterView.count)
     }
 
+    //MARK: Actions
     @IBAction func pushButtonPressed(_ pushButton: PushButton) {
         if pushButton.isAddButton {
             if counterView.count != 8 {
@@ -31,6 +34,19 @@ class ViewController: UIViewController {
             counterView.count -= 1
         }
         counterLabel.text = String(counterView.count)
+        
+        if (isGraphViewShowing) {
+            counterViewTap(nil)
+        }
+    }
+    @IBAction func counterViewTap(_ gesture: UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            //hide graph
+            UIView.transition(from: graphView, to: counterView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        } else {
+            //show graph
+            UIView.transition(from: counterView, to: graphView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+        }
     }
     
 }
