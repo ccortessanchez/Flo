d72555c92be1c7c4c09890a2e9f575beabc30052
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-09-26 17:20:54 +0200

    Add markers for counter view

--- Flo/Flo/CounterView.swift
+++ Flo/Flo/CounterView.swift
@@ -72,6 +72,43 @@ import UIKit
         outlineColor.setStroke()
         outlinePath.lineWidth = Constants.lineWidth
         outlinePath.stroke()
+        
+        //Counter View markers
+        let context = UIGraphicsGetCurrentContext()
+        
+        //save original state
+        context?.saveGState()
+        outlineColor.setFill()
+        
+        let markerWidth: CGFloat = 5.0
+        let markerSize: CGFloat = 10.0
+        
+        //marker rectangle positioned at the top left
+        let markerPath = UIBezierPath(rect: CGRect(x: -markerWidth / 2, y: 0, width: markerWidth, height: markerSize))
+        
+        //move top left of context to the previous center position
+        context?.translateBy(x: rect.width / 2, y: rect.height / 2)
+        
+        for i in 1...Constants.numberOfGlasses {
+            //save the centred context
+            context?.saveGState()
+            
+            //calculate the rotation angle
+            let angle = arcLengthPerGlass * CGFloat(i) + startAngle - .pi / 2
+            
+            //rotate and translate
+            context?.rotate(by: angle)
+            context?.translateBy(x: 0, y: rect.height / 2 - markerSize)
+            
+            //fill the marker rectangle
+            markerPath.fill()
+            
+            //restore the centred context for the next rotate
+            context?.restoreGState()
+        }
+        
+        //restore the original state
+        context?.restoreGState()
     }
 
 }
