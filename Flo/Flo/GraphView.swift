ae51f16a2c93416d3952282eb4a03114da2f6fb8
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-09-26 16:16:06 +0200

    Add horizontal lines to graph

--- Flo/Flo/GraphView.swift
+++ Flo/Flo/GraphView.swift
@@ -126,6 +126,27 @@ import UIKit
             let circle = UIBezierPath(ovalIn: CGRect(origin: point, size: CGSize(width: Constants.circleDiameter, height: Constants.circleDiameter)))
             circle.fill()
         }
+        
+        //Draw horizontal graph lines on top of everything
+        let linePath = UIBezierPath()
+        
+        //top line
+        linePath.move(to: CGPoint(x: margin, y: topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: topBorder))
+        
+        //center line
+        linePath.move(to: CGPoint(x: margin, y: graphHeight/2 + topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: graphHeight/2 + topBorder))
+        
+        //bottom line
+        linePath.move(to: CGPoint(x: margin, y: height - bottomBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: height - bottomBorder))
+        
+        let color = UIColor(white: 1.0, alpha: Constants.colorAlpha)
+        color.setStroke()
+        
+        linePath.lineWidth = 1.0
+        linePath.stroke()
     }
 
 }
