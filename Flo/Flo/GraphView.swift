138b5d0294af6d783c270ab8a83e008a43eac34d
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-09-26 12:31:44 +0200

    Add clipping to graph view

--- Flo/Flo/GraphView.swift
+++ Flo/Flo/GraphView.swift
@@ -14,7 +14,21 @@ import UIKit
     @IBInspectable var startColor: UIColor = .red
     @IBInspectable var endColor: UIColor = .green
 
+    private struct Constants {
+        static let cornerRadiusSize = CGSize(width: 8.0, height: 8.0)
+        static let margin: CGFloat = 20.0
+        static let topBorder: CGFloat = 60.0
+        static let bottomBorder: CGFloat = 50.0
+        static let colorAlpha: CGFloat = 0.3
+        static let circleDiameter: CGFloat = 5.0
+    }
+    
+    
     override func draw(_ rect: CGRect) {
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: .allCorners, cornerRadii: Constants.cornerRadiusSize)
+        path.addClip()
+        
         //obtain current context
         //CG drawing methods need a context in order to draw
         let context = UIGraphicsGetCurrentContext()
